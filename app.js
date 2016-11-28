69386c1790e0cad7295c6fcc5ef13fc5bc4d76d2
Jing0017 <564752135@qq.com>
2016-11-28 20:18:58 +0800

    add .

--- app.js
+++ app.js
@@ -1,153 +1,55 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var cookieSession = require('cookie-session')
+var session = require('express-session')
+var mongoStore = require('connect-mongo')(session)
 var path = require('path')
 var mongoose = require('mongoose')
-var _ = require('underscore')
-var Movie = require('./models/movie')
+var logger = require('morgan')
+
 var port = process.env.PORT || 3000
 var app = express()
+var dbUrl = 'mongodb://localhost/imooc'
 
-mongoose.connect('mongodb://localhost/imooc')
+mongoose.connect(dbUrl)
 
-app.set('views', './views/pages')
+//allow custom header and CORS
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); //让options请求快速返回
+  } else {
+    next()
+  }
+})
+
+app.set('views', './app/views/pages')
 app.set('view engine', 'jade')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(session({
+  secret: 'imooc',
+  store: new mongoStore({
+    url: dbUrl,
+    collection: 'sessions'
+  })
+}))
+
+if ('development' === app.get('env')) {
+  app.set('showStackError', true)
+  app.use(logger(':method :url :status'))
+  app.locals.pretty = true
+  mongoose.set('debug', true)
+}
+
+require('./config/routes')(app)
+
 app.locals.moment = require('moment')
 app.use(express.static(path.join(__dirname, 'public')))
 app.listen(port)
 
 console.log('imooc start on port ' + port)
-
-//index page
-app.get('/', function(req, res){
-	Movie.fetch(function(err, movies){
-		if(err){
-			console.log(err)
-		}
-		res.render('index', {
-			title: 'imooc 首页',
-			movies: movies
-		})
-	})
-})
-
-
-//detail page
-app.get('/movie/:id', function(req, res){
-	var id = req.params.id
-	Movie.findById(id, function(err, movie){
-		res.render('detail', {
-			title:'imooc ' + movie.title,
-			movie: movie
-		})
-	})
-})
-
-//admin	 page
-app.get('/admin/movie', function(req, res){
-	res.render('admin', {
-		title:'imooc 后台录入页',
-		movie:{
-			title:'',
-			doctor:'',
-			country:'',
-			language:'',
-			year:'',
-			poster:'',
-			summary:'',
-			flash:''
-		}
-	})
-})
-
-//admin update movie
-app.get('/admin/update/:id', function (req, res) {
-	var id = req.params.id
-
-	if(id){
-		Movie.findById(id, function (err, movie) {
-			res.render('admin',{
-				title:'imooc 后台更新页',
-				movie:movie
-			})
-		})
-	}
-})
-
-// admin post movie
-app.post('/admin/movie/new', function(req, res){
-	console.log(req.body)
-	var id = req.body.movie._id
-	var movieObj = req.body.movie
-	var  _movie
-
-	if(id !== 'undefined'){
-		Movie.findById(id, function (err, movie) {
-			if(err){
-				console.log(err)
-			}
-			_movie = _.extend(movie, movieObj)
-			_movie.save(function(err, movie){
-				if(err){
-					console.log(err)
-				}
-
-				res.redirect('/movie/' + movie._id)
-			})
-		})
-	}else{
-		_movie = new Movie({
-			doctor: movieObj.doctor,
-			title: movieObj.title,
-			country: movieObj.country,
-			language: movieObj.language,
-			year: movieObj.year,
-			poster: movieObj.poster,
-			summary: movieObj.summary,
-			flash:movieObj.flash
-		})
-
-		_movie.save(function(err, movie){
-			if(err){
-				console.log(err)
-			}
-
-			res.redirect('/movie/' + movie._id)
-		})
-	}
-})
-
-//list page
-app.get('/admin/list', function(req, res){
-
-	Movie.fetch(function(err, movies){
-		if(err){
-			console.log(err)
-		}
-		res.render('list', {
-			title:'imooc 列表页',
-			movies: movies
-		})
-	})
-})
-
-// list delete movie
-app.delete('/admin/list', function(req, res){
-	var id = req.query.id
-
-	if(id){
-		Movie.remove({_id:id},function (err, movie) {
-			if(err){
-				console.list(err)
-			}else{
-				res.json({success:1})
-			}
-		})
-	}
-})
-
-
-
-
-
-
